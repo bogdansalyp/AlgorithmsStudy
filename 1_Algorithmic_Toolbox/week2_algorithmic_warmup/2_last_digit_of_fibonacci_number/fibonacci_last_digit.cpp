399dc7b97f71d02b695d183d9f1ded3faab83cc0
bsalyp <bsalyp@cisco.com>
2019-08-10 19:31:21 +0300

    Solved Algorithms 2.2 problem

--- 1_Algorithmic_Toolbox/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
+++ 1_Algorithmic_Toolbox/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 int get_fibonacci_last_digit_naive(int n) {
     if (n <= 1)
@@ -16,9 +17,22 @@ int get_fibonacci_last_digit_naive(int n) {
     return current % 10;
 }
 
+int fibonacci_last_digit_fast(int n) {
+    std::vector<int> arr;
+    arr.reserve(n + 1);
+
+    arr[0] = 0;
+    arr[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        arr[i] = (arr[i - 1] + arr[i - 2]) % 10;
+    }
+
+    return arr[n];
+}
+
 int main() {
     int n;
     std::cin >> n;
-    int c = get_fibonacci_last_digit_naive(n);
+    int c = fibonacci_last_digit_fast(n);
     std::cout << c << '\n';
     }
