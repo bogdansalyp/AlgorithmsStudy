29079af2f46d1242a0974306e750868fc5ac9944
bsalyp <bsalyp@cisco.com>
2019-08-10 19:46:39 +0300

    Solved Algorithms 2.4 problem

--- 1_Algorithmic_Toolbox/week2_algorithmic_warmup/4_least_common_multiple/lcm.cpp
+++ 1_Algorithmic_Toolbox/week2_algorithmic_warmup/4_least_common_multiple/lcm.cpp
@@ -8,9 +8,33 @@ long long lcm_naive(int a, int b) {
   return (long long) a * b;
 }
 
+int gcd_fast(int a, int b) {
+  if (a < b) {
+    int temp = a;
+    a = b;
+    b = temp;
+  }
+  int remainder = a % b;
+  if (remainder == 0) {
+    return b;
+  } else {
+    return gcd_fast(b, remainder);
+  }
+}
+
+long long lcm_fast(int a, int b) {
+  if ((a == 0) || (b == 0)) {
+    return 0;
+  }
+  int gcd = gcd_fast(a, b);
+  int a_prime = a / gcd;
+  int b_prime = b / gcd;
+  return (long long) gcd * a_prime * b_prime;
+}
+
 int main() {
   int a, b;
   std::cin >> a >> b;
-  std::cout << lcm_naive(a, b) << std::endl;
+  std::cout << lcm_fast(a, b) << std::endl;
   return 0;
 }
