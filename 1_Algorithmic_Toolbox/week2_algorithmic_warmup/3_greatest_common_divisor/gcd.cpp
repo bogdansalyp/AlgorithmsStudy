c8d8f075aec98fe0913d82028c23d501f55685b7
bsalyp <bsalyp@cisco.com>
2019-08-10 19:38:12 +0300

    Solved Algorithms 2.3 problem

--- 1_Algorithmic_Toolbox/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.cpp
+++ 1_Algorithmic_Toolbox/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.cpp
@@ -12,9 +12,24 @@ int gcd_naive(int a, int b) {
   return current_gcd;
 }
 
+int gcd_fast(int a, int b) {
+  if (a < b) {
+    int temp = a;
+    a = b;
+    b = temp;
+  }
+  int remainder = a % b;
+  if (remainder == 0) {
+    return b;
+  } else {
+    return gcd_fast(b, remainder);
+  }
+}
+
 int main() {
   int a, b;
   std::cin >> a >> b;
-  std::cout << gcd_naive(a, b) << std::endl;
+  // std::cout << gcd_naive(a, b) << std::endl;
+  std::cout << gcd_fast(a, b) << std::endl;
   return 0;
 }
