e57e682efba4daedf7cb8c9e1b52faf1a39d6ca5
bsalyp <bsalyp@cisco.com>
2019-08-10 19:19:02 +0300

    Solved Algorithms 1.2 problem

--- 1_Algorithmic_Toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
+++ 1_Algorithmic_Toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
@@ -1,17 +1,46 @@
+#include <cstdlib>
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
-int MaxPairwiseProduct(const std::vector<int>& numbers) {
-    int max_product = 0;
+long long MaxPairwiseProductHard(const std::vector<int>& numbers) {
+    long long max_product = 0;
     int n = numbers.size();
 
     for (int first = 0; first < n; ++first) {
         for (int second = first + 1; second < n; ++second) {
             max_product = std::max(max_product,
-                numbers[first] * numbers[second]);
+                (long long)numbers[first] * (long long)numbers[second]);
         }
     }
+
+    return max_product;
+}
+
+long long MaxPairwiseProduct(const std::vector<int>& numbers) {
+    long long max_product = 0;
+    int n = numbers.size();
+
+    int index_1;
+    int index_2;
+    if (numbers[0] > numbers[1]) {
+        index_1 = 0;
+        index_2 = 1;
+    } else {
+        index_2 = 0;
+        index_1 = 1;
+    }
+
+    for (int i = 2; i < n; i++) {
+        if (numbers[i] > numbers[index_1]) {
+            index_2 = index_1;
+            index_1 = i;
+        } else if (numbers[i] > numbers[index_2]) {
+            index_2 = i;
+        }
+    }
+
+    max_product = (long long)numbers[index_1] * (long long)numbers[index_2];
 
     return max_product;
 }
@@ -24,6 +53,31 @@ int main() {
         std::cin >> numbers[i];
     }
 
-    std::cout << MaxPairwiseProduct(numbers); << "\n";
+    // while (true) {
+    //     long long result_1;
+    //     long long result_2;
+
+    //     int n = rand() % 10 + 2;
+    //     std::vector<int> a;
+    //     for (int i = 0; i < n; i++) {
+    //         a.push_back(rand() % 10000);
+    //     }
+
+    //     for (int i = 0; i < n; i++) {
+    //         std::cout << a[i] << " ";
+    //     }
+    //     std::cout << "\n";
+
+    //     result_1 = MaxPairwiseProductHard(a);
+    //     result_2 = MaxPairwiseProduct(a);
+
+    //     if (result_1 != result_2) {
+    //         std::cout << "Wrong answer: " << result_1 << " " << result_2 << "\n";
+    //         return 0;
+    //     }
+        
+    //     std::cout << "OK\n";
+    // }
+    std::cout << MaxPairwiseProduct(numbers) << "\n";
     return 0;
 }
