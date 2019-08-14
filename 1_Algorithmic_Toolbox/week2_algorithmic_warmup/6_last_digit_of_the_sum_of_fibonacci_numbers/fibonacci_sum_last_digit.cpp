be6c7d2b7eb4587ff18f31a8696eee04ddb289f8
bsalyp <bsalyp@cisco.com>
2019-08-14 15:22:15 +0300

    Solved Algorithms 2.6 problem

--- 1_Algorithmic_Toolbox/week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.cpp
+++ 1_Algorithmic_Toolbox/week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 int fibonacci_sum_naive(long long n) {
     if (n <= 1)
@@ -18,8 +19,24 @@ int fibonacci_sum_naive(long long n) {
     return sum % 10;
 }
 
+int fibonacci_sum_fast(long long n) {
+    long long numbers_to_count = n % 60;
+    if (numbers_to_count == 0) {
+        return 0;
+    }
+    std::vector<int> arr(numbers_to_count + 1, 0);
+    arr[0] = 0;
+    arr[1] = 1;
+    long long sum = 1;
+    for (long long i = 2; i <= numbers_to_count; i++) {
+        arr[i] = (arr[i - 1] + arr[i - 2]) % 10;
+        sum = (sum + arr[i]) % 10;
+    }
+    return sum;
+}
+
 int main() {
     long long n = 0;
     std::cin >> n;
-    std::cout << fibonacci_sum_naive(n);
+    std::cout << fibonacci_sum_fast(n);
 }
