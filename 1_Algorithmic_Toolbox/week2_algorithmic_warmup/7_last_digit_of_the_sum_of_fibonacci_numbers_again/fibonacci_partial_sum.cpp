c1af0d2867e1fa58937408fd86417d5873da9180
bsalyp <bsalyp@cisco.com>
2019-08-14 15:22:30 +0300

    Solved Algorithms 2.7 problem

--- 1_Algorithmic_Toolbox/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.cpp
+++ 1_Algorithmic_Toolbox/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.cpp
@@ -21,8 +21,39 @@ long long get_fibonacci_partial_sum_naive(long long from, long long to) {
     return sum % 10;
 }
 
+int fibonacci_sum_fast(long long n) {
+    long long numbers_to_count = n % 60;
+    if (numbers_to_count == 0) {
+        return 0;
+    }
+    std::vector<int> arr(numbers_to_count + 1, 0);
+    arr[0] = 0;
+    arr[1] = 1;
+    long long sum = 1;
+    for (long long i = 2; i <= numbers_to_count; i++) {
+        arr[i] = (arr[i - 1] + arr[i - 2]) % 10;
+        sum = (sum + arr[i]) % 10;
+    }
+    return sum;
+}
+
+long long get_fibonacci_partial_sum_fast(long long from, long long to) {
+    if (from == 0) {
+        return fibonacci_sum_fast(to);
+    }
+    long long from_sum = fibonacci_sum_fast(from - 1);
+    long long to_sum = fibonacci_sum_fast(to);
+
+    if (to_sum < from_sum) {
+        to_sum += 10;
+    } 
+    long long sum = to_sum - from_sum;
+
+    return sum;
+}
+
 int main() {
     long long from, to;
     std::cin >> from >> to;
-    std::cout << get_fibonacci_partial_sum_naive(from, to) << '\n';
+    std::cout << get_fibonacci_partial_sum_fast(from, to) << '\n';
 }
