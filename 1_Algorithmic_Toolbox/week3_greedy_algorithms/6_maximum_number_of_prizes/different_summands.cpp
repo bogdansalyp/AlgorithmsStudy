74df6eb825a0eb4a4d674529fb38e5649b75e440
bsalyp <bsalyp@cisco.com>
2019-08-15 21:36:19 +0300

    Solved Algorithms 3.6 problem

--- 1_Algorithmic_Toolbox/week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.cpp
+++ 1_Algorithmic_Toolbox/week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.cpp
@@ -5,7 +5,17 @@ using std::vector;
 
 vector<int> optimal_summands(int n) {
   vector<int> summands;
-  //write your code here
+  int current_amount = 0;
+  while(n > 0) {
+    current_amount++;
+    if (n - current_amount < current_amount + 1) {
+      summands.push_back(n);
+      return summands;
+    } else {
+      summands.push_back(current_amount);
+    }
+    n -= current_amount;
+  }
   return summands;
 }
 
