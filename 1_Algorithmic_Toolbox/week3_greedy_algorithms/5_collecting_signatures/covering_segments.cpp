5cda9580ad4bde3100d524d7e2b06b6d061cbf0c
bsalyp <bsalyp@cisco.com>
2019-08-15 21:35:57 +0300

    Solved Algorithms 3.5 problem

--- 1_Algorithmic_Toolbox/week3_greedy_algorithms/5_collecting_signatures/covering_segments.cpp
+++ 1_Algorithmic_Toolbox/week3_greedy_algorithms/5_collecting_signatures/covering_segments.cpp
@@ -11,11 +11,26 @@ struct Segment {
 
 vector<int> optimal_points(vector<Segment> &segments) {
   vector<int> points;
-  //write your code here
-  for (size_t i = 0; i < segments.size(); ++i) {
-    points.push_back(segments[i].start);
-    points.push_back(segments[i].end);
+  while(segments.size() != 0) {
+    // find a minimum end
+    int min_end = 0;
+    for (int i = 1; i < segments.size(); i++) {
+      if (segments[i].end < segments[min_end].end) {
+        min_end = i;
+      }
+    }
+    points.push_back(segments[min_end].end);
+
+    // delete all used segments
+    vector<Segment> temp_segments;
+    for (int i = 0; i < segments.size(); i++) {
+      if ((segments[i].end < segments[min_end].end) || (segments[i].start > segments[min_end].end)) {
+        temp_segments.push_back(segments[i]);
+      }
+    }
+    segments = temp_segments;
   }
+  
   return points;
 }
 
