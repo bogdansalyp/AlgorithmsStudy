3c5a4ba18bd2196c1f061bcb5d70956c12637ce1
bsalyp <bsalyp@cisco.com>
2019-08-15 21:35:37 +0300

    Solved Algorithms 3.4 problem

--- 1_Algorithmic_Toolbox/week3_greedy_algorithms/4_maximum_advertisement_revenue/dot_product.cpp
+++ 1_Algorithmic_Toolbox/week3_greedy_algorithms/4_maximum_advertisement_revenue/dot_product.cpp
@@ -4,9 +4,24 @@
 
 using std::vector;
 
+vector<int> bubble_sort(vector<int> arr) {
+  for(int i = 0; i < arr.size() - 1; i++) {
+    for(int j = i; j < arr.size(); j++) {
+      if(arr[i] < arr[j]) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
 long long max_dot_product(vector<int> a, vector<int> b) {
   // write your code here
   long long result = 0;
+  a = bubble_sort(a);
+  b = bubble_sort(b);
   for (size_t i = 0; i < a.size(); i++) {
     result += ((long long) a[i]) * b[i];
   }
