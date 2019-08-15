f9422de2d338c64e3a94b548ff3312408234ea0a
bsalyp <bsalyp@cisco.com>
2019-08-15 21:33:31 +0300

    Solved Algorithms 3.1 problem

--- 1_Algorithmic_Toolbox/week3_greedy_algorithms/1_money_change/change.cpp
+++ 1_Algorithmic_Toolbox/week3_greedy_algorithms/1_money_change/change.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 
 int get_change(int m) {
-  //write your code here
+  int n = 0;
+  while(m > 0) {
+    if (m >= 10) {
+      m -= 10;
+      n++;
+    } else if (m >= 5) {
+      m -= 5;
+      n++;
+    } else {
+      m -= 1;
+      n++;
+    }
+  }
   return n;
 }
 
